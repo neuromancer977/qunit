b26464a5b3f8d451bb7efffaf27c5895d4d95582
neuromancer977 <neuromancer977@gmail.com>
2017-01-03 11:58:29 +0100

    Test: update

--- dist/tests.js
+++ dist/tests.js
@@ -1,7 +1,7 @@
 //import QUnit from "qunit.js";
 
-QUnit.module('module 1');
+QUnit.module( 'module 1' );
 
-QUnit.test( "hello test", function( assert) {
+QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
-});
+} );
